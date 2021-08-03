d5b7034b12a82b46f51653184e07ebcafa68b63b
Guilherme Alvaro Dos Santos <87342872+gelherme@users.noreply.github.com>
2021-08-02 22:52:35 -0300

    botões de acrescentar e diminuir terminados

--- js/script.js
+++ js/script.js
@@ -1,68 +1,43 @@
-// bugs
-// [minutos] quando solicitado para aumentar logo depois de diminuir, é diminuido + uma casa.(tentar usar looping)
+let acrecentaMin = document.getElementById('maisEsq');
+let acrecentaSeg = document.getElementById('maisDir');
+let diminuiMin = document.getElementById('menosEsq');
+let diminuiSeg = document.getElementById('menosDir');
+let interfaceMin = document.getElementById('minutos');
+let interfaceSeg = document.getElementById('segundos');
 
-let minutos = document.getElementById('minutos').innerHTML;
-let segundos = document.getElementById('segundos').innerHTML;
+//terminar função
+function iniciarCronometro(){
+    let min = interfaceMin.innerHTML;
+    let seg = interfaceSeg.innerHTML;
 
-//function onclick para somar os minutos
+    setTimeout(()=>{for(i = interfaceSeg.innerHTML ; i >= 0; i--){
+        interfaceSeg.innerHTML=i;
+    }},1000);
+}
+
+
+
+
 function somaMinuto(){
-    if(minutos < "10"){
-    document.getElementById('minutos').innerHTML = '0'+minutos ++}else{
-        document.getElementById('minutos').innerHTML = minutos ++
-    }
+    if(interfaceMin.innerHTML < 99){
+ interfaceMin.innerHTML++;}
+   
 }
-
-
-//function onclick para diminuir os minutos
-
-function diminuiMinuto(){
-    if(minutos < "00"){
-        return "00"
-    }else if(minutos < "10" && minutos > "-1"){
-        document.getElementById('minutos').innerHTML = '0'+minutos --}else{
-            document.getElementById('minutos').innerHTML = minutos --
-}
-}
-
-//function onclick para somar os segundos
 
 function somaSegundo(){
+    if(interfaceSeg.innerHTML < 59){
+    interfaceSeg.innerHTML++;}
+      
+  }
 
-    if(segundos < "10"){
-        document.getElementById('segundos').innerHTML = '0' + segundos ++
-    }else if(segundos > "-1" && segundos < "60"){
-        document.getElementById('segundos').innerHTML = segundos ++
-    }
-}
-
-//function onclick para diminuir os segundos
-
+function diminuiMinuto(){
+    if(interfaceMin.innerHTML > 0){
+    interfaceMin.innerHTML--;}
+      
+   }
 
 function diminuiSegundo(){
-    if(segundos < "00"){
-        return "00"
-    }else if(segundos < "10"){
-        document.getElementById('segundos').innerHTML = '0'+segundos --}else if(segundos >= "00" && segundos < "59"){
-            document.getElementById('segundos').innerHTML = segundos --
-}
-}
-
-//function iniciar cronometro
-/*arrumar*/ 
-function iniciarCronometro(){
-    function iniciarSegundos(){
-        for( var segundos = document.getElementById("segundos").innerHTML;segundos >"00";segundos--){
-            if(minutos > "00"){
-                minutos--;
-                segundos = "59";
-              // codigo para debugar  console.log(segundos)
-            }else{
-                minutos = "00";
-            }
-        }
-    }
-    iniciarSegundos();
-}
-
-
-
+    if(interfaceSeg.innerHTML > 0){
+        interfaceSeg.innerHTML--;}
+      
+   }
